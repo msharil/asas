e627e917250b0be32d2cf4fddcbfbe43aae0833d
msharil <msharil1983@gmail.com>
2014-01-24 11:43:23 +0800

    Final training. #dialog.html - Masih ada bug #iframe.html - not tested

---
+++ form.php
@@ -0,0 +1,8 @@
+<?php
+if($_POST["hantar"]){
+	$nama=$_POST["nama"];
+	$v1=$_POST["v1"];
+	$v2=$_POST["v2"];
+	echo $nama." v1 = ".$v1.", v2 = ".$v2;
+}
+?>
---
+++ form2.php
@@ -0,0 +1,18 @@
+<?php
+	$nogaji=$_POST["nogaji"];
+	$name=$_POST["name"];
+	$rbSex=$_POST["rbSex"];
+	$tkhlahir=$_POST["tkhlahir"];
+	$rbSektor=$_POST["rbSektor"];
+	$selBhgn=$_POST["selBhgn"];
+	$skill=$_POST["skill"];
+
+	echo "No. Gaji: ".$nogaji.'<br />';
+	echo "Nama: ".$name.'<br />';
+	echo "Jantina: ".$rbSex.'<br />';
+	echo "Tarikh Lahir: ".$tkhlahir.'<br />';
+	echo "Sektor: ".$rbSektor.'<br />';
+	echo "Bahagian: ".$selBhgn.'<br />';	
+	echo "Skill: ".$skill.'<br />';
+	
+?>
